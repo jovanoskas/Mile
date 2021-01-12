3ea60eab95f2f7a82449e0b10c304a97d729a9ff
Stefani <sjovanoska@outlook.com>
2021-01-12 16:54:18 +0100

    frontend added!

--- src/main/java/com/example/demo/controllers/CoursesController.java
+++ src/main/java/com/example/demo/controllers/CoursesController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("/courses")
 public class CoursesController {
 
     private final CoursesService coursesService;
--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 @RequestMapping
 public class HomeController {
 
